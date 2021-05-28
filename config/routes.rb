6e4b4bfb802af79b6c3b9498060484bb1c705eba
MIchael <miklblitz@yandex.ru>
2021-05-28 04:53:02 +0400

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'goods#index'
-  resources :favorites, except: [:index] do
+  resources :favorites, except: [:index, :destroy, :show, :edit, :new] do
     collection do
       get :goods
       get :get_data
       delete '/delete_by_favorite/:goods_key', to: 'favorites#delete_by_favorite'
     end
   end
-  get '/goods/run_job', to: 'goods#run_job' # для имитации крона с https://cron-job.org/ - запросы идут оттуда
-  resources :goods, only: [:show]
+  resources :goods, only: [:show] do
+    collection do
+      get :run_job # для имитации крона с https://cron-job.org/ - запросы идут оттуда
+    end
+  end
 end
